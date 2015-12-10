ab3fd7f47845b172487b08d3fa025ecb1602e2c6
thbeucher <beucherlamacque.thomas@gmail.com>
2015-12-10 17:46:48 +0100

    added C5

--- MotorControlModel/Plot/plotFunctions.py
+++ MotorControlModel/Plot/plotFunctions.py
@@ -699,8 +699,8 @@ def plotScattergram(what,foldername):
         for i in range(len(rs.sizeOfTarget)):
             ax = plt.subplot2grid((2,2), (i/2,i%2))
             ax.hist(data[rs.sizeOfTarget[i]], 20)
-            ax.plot([-rs.sizeOfTarget[i], -rs.sizeOfTarget[i]], [0, 500], c = 'r', linewidth = 3)
-            ax.plot([rs.sizeOfTarget[i], rs.sizeOfTarget[i]], [0, 500], c = 'r', linewidth = 3)
+            ax.plot([-rs.sizeOfTarget[i]/2, -rs.sizeOfTarget[i]]/2, [0, 500], c = 'r', linewidth = 3)
+            ax.plot([rs.sizeOfTarget[i]/2, rs.sizeOfTarget[i]]/2, [0, 500], c = 'r', linewidth = 3)
             ax.set_title(str("Hit Dispersion for Target " + str(rs.sizeOfTarget[i])))
 
     elif what=="RBFN":
@@ -712,8 +712,8 @@ def plotScattergram(what,foldername):
                     tabx.append(el[j])
             plt.hist(tabx, 20)
             for i in range(len(rs.sizeOfTarget)):
-                plt.plot([-rs.sizeOfTarget[i], -rs.sizeOfTarget[i]], [0, 20], c = 'r', linewidth = 3)
-                plt.plot([rs.sizeOfTarget[i], rs.sizeOfTarget[i]], [0, 20], c = 'r', linewidth = 3)
+                plt.plot([-rs.sizeOfTarget[i]/2, -rs.sizeOfTarget[i]]/2, [0, 20], c = 'r', linewidth = 3)
+                plt.plot([rs.sizeOfTarget[i]/2, rs.sizeOfTarget[i]]/2, [0, 20], c = 'r', linewidth = 3)
             plt.xlabel("X (m)")
             plt.ylabel("Y (m)")
             plt.title("Hit Dispersion for RBFN")
