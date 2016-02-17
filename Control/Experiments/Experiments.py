d37c3b614ca24b5d960452c368a1d3ffcbb71d99
Corentin <corentin.arnaud9@gmail.com>
2016-02-17 11:19:27 +0100

    fix cost map issue

--- Control/Experiments/Experiments.py
+++ Control/Experiments/Experiments.py
@@ -17,7 +17,7 @@ from Utils.Chrono import Chrono
 from GlobalVariables import pathDataFolder
 
 from TrajMaker import TrajMaker
-from Utils.FileWritting import checkIfFolderExists, findDataFilename
+from Utils.FileWritting import checkIfFolderExists, findDataFilename, writeNumpy
 
 #------------------------------------------------------------------------------
 
@@ -65,13 +65,13 @@ class Experiments:
         filename = findDataFilename(self.foldername+"Cost/","traj",".cost")
         filenameTime = findDataFilename(self.foldername+"TrajTime/","traj",".time")
         filenameX = findDataFilename(self.foldername+"finalX/","x",".last")
-        '''
-        filename=self.foldername+"Cost/traj.cost"
-        filenameTime=self.foldername+"TrajTime/traj.time"
-        filenameX=self.foldername+"finalX/x.cost"
         np.savetxt(filename, self.costStore)
         np.savetxt(filenameTime, self.trajTimeStore)
         np.savetxt(filenameX, self.lastCoord)
+        '''
+        writeNumpy(self.costStore,self.foldername+"Cost/","traj",".cost")
+        writeNumpy(self.trajTimeStore, self.foldername+"TrajTime/","traj",".time")
+        writeNumpy(self.lastCoord, self.foldername+"finalX/","x",".last")
          
     def runOneTrajectory(self, x, y):
         #self.tm.saveTraj = True
