55e0897675a521ea46f849fb636ce4537598370a
Olivier Sigaud <olivier.sigaud@upmc.fr>
2016-08-09 11:43:43 +0200

    changed cluster files configuration to perform gamma study

--- Control/ScriptClusterOneTargetNController.sh
+++ Control/ScriptClusterOneTargetNController.sh
@@ -2,8 +2,8 @@
 #PBS -N arm_model$TARGET
 #PBS -o arm_model$TARGET.out
 #PBS -b arm_model$TARGET.err
-#PBS -l walltime=60:00:00
+#PBS -l walltime=48:00:00
 #PBS -l ncpus=15
 #PBS -V
 #PBS -d /home/corentin.arnaud/stage/ArmModelPython/Control/
-python clusterOneTargetNController.py setupK10gamma3.xml $TARGET
+python clusterOneTargetNController.py setupK10gamma6.xml $TARGET
