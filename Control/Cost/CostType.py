34f894715a3fe49eafd1ba1ebb8bcec87f8bfc6c
Olivier Sigaud <olivier.sigaud@upmc.fr>
2016-08-09 17:45:54 +0200

    ajout cost files

---
+++ Control/Cost/CostType.py
@@ -0,0 +1,5 @@
+from CostDDPG import CostDDPG
+from CostCMAESNoPer import CostCMAESNoPer
+from CostCMAES import CostCMAES
+
+CostType={'CostCMAES':CostCMAES, 'CostDDPG':CostDDPG,'CostCMAESNoPer':CostCMAESNoPer}
