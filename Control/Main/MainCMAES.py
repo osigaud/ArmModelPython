54a59b35e468c706cfd84a9dbd07f479698e0cdb
Olivier Sigaud <olivier.sigaud@upmc.fr>
2016-08-22 09:54:49 +0200

    cleaning up

---
+++ Control/Main/MainCMAES.py
@@ -0,0 +1,335 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: Corentin Arnaud + Olivier Sigaud
+
+Module: Main
+
+Description: useful functions to run cmaes and some scripts to run trajectories
+'''
+#TODO: Make a optimisation and CMAES class
+import os
+import cma
+from shutil import copyfile
+from functools import partial
+from multiprocess.pool import ThreadPool
+import numpy as np
+from GlobalVariables import pathDataFolder
+from Utils.Chrono import Chrono
+
+from Experiments.Experiments import Experiments
+from Utils.FileWriting import checkIfFolderExists
+
+def copyRegressiontoCMAES(rs, name, size):
+    cmaname =  rs.OPTIpath + str(size) + "/"
+    checkIfFolderExists(cmaname)
+    savenametheta = rs.path + name + ".theta"
+    copyfile(savenametheta, cmaname + name + ".theta")
+    
+    if(rs.regression=="RBFN"):
+        savenamestruct = rs.path + name + ".struct"
+        copyfile(savenamestruct, cmaname + name + ".struct")
+
+def GenerateDataFromTheta(rs, target_size, foldername, thetaFile, repeat, save):
+    '''
+    Generate Data from a given theta file
+
+    Input:    -target_size, size of the target, float
+            -save: do we save the results (false when running CMAES)? True = Yes, False = No
+    '''
+    os.system("rm "+foldername+"Log/*.log 2>/dev/null")
+    exp = Experiments(rs, target_size, save, foldername,thetaFile,rs.popsizeCmaes,rs.period)
+    cost, time = exp.runTrajectoriesForResultsGeneration(repeat)
+    print("Average cost: ", cost)
+    print("Average time: ", time)
+    print("foldername : ", foldername)
+    if (save):
+        exp.saveCost()
+        
+def GenerateDataFromThetaNController(rs, target_size, foldername, thetaFile, repeat, save,noise=None):
+    '''
+    Generate Data from a given theta file
+
+    Input:    -target_size, size of the target, float
+            -save: do we save the results (false when running CMAES)? True = Yes, False = No
+    '''
+    os.system("rm "+foldername+"/Log/*.log 2>/dev/null")
+    exp = Experiments(rs, target_size, save, foldername,None,rs.popsizeCmaes,rs.period)
+    if(noise!=None): exp.setNoise(noise)
+    cost, time = exp.runTrajectoriesForResultsGenerationNController(repeat,thetaFile)
+    print("Average cost: ", cost)
+    print("Average time: ", time)
+    print("foldername : ", foldername)
+    if (save):
+        exp.saveCost()
+        
+def GenerateDataFromThetaOnePoint(rs, target_size, foldername, thetaFile, repeat, point):
+    '''
+    Generate Data from a given theta file
+
+    Input:    -target_size, size of the target, float
+            -save: do we save the results (false when running CMAES)? True = Yes, False = No
+    '''
+    os.system("rm "+foldername+"/Log/*.log 2>/dev/null")
+    exp = Experiments(rs, target_size, True, foldername,thetaFile,rs.popsizeCmaes,rs.period)
+    cost, time = exp.runTrajectoriesForResultsGenerationOnePoint(repeat,point)
+    print("Average cost: ", cost)
+    print("Average time: ", time)
+    print("foldername : ", foldername)
+
+
+def GenerateRichDataFromTheta(rs, target_size, foldername, thetaFile, repeat, save):
+    '''
+    Generate Data from a given theta file
+
+    Input:    -target_size, size of the target, float
+            -save: do we save the results (false when running CMAES)? True = Yes, False = No
+    '''
+    os.system("rm "+foldername+"Log/*.log")
+    exp = Experiments(rs, target_size, save, foldername,thetaFile,rs.popsizeCmaes,rs.period)
+    cost = exp.runRichTrajectories(repeat)
+    print("Average cost: ", cost)
+    print("foldername : ", foldername)
+    if (save):
+        exp.saveCost()
+
+def generateFromCMAES(repeat, rs, thetaFile, saveDir = 'Data'):
+    for el in rs.target_size:
+        c = Chrono()
+        thetaName = rs.OPTIpath + str(el) + "/" + thetaFile
+        saveName = rs.OPTIpath + str(el) + "/" + saveDir + "/"
+        GenerateDataFromTheta(rs,el,saveName,thetaName,repeat,True)
+        c.stop()
+    print("CMAES:End of generation")
+    
+def generateFromCMAESNController(repeat, rs, thetaFile, saveDir = 'Data', noise=None):
+    for el in rs.target_size:
+        c = Chrono()
+        thetaName = rs.OPTIpath + str(el)+"/*/"+thetaFile
+        saveName = rs.OPTIpath + str(el) + "/" + saveDir + "/"
+        GenerateDataFromThetaNController(rs,el, saveName,thetaName,repeat,True,noise)
+        c.stop()
+    print("CMAES:End of generation")
+    
+def generateFromCMAESonePoint(repeat, rs, thetaFile, saveDir = 'Data', size=0.04, point=0):
+    c = Chrono()
+    thetaName = rs.OPTIpath + str(size)+"/"+str(point)+"/"+thetaFile
+    saveName = rs.OPTIpath + str(size) + "/" + saveDir + "/"
+    GenerateDataFromThetaOnePoint(rs,size, saveName,thetaName,repeat, point)
+    c.stop()
+    print("CMAES:End of generation")
+
+def generateRichDataFromCMAES(repeat, rs, thetaFile, saveDir = 'Data'):
+    for el in rs.target_size:
+        thetaName = rs.OPTIpath + str(el) + "/" + thetaFile
+        saveName = rs.OPTIpath + str(el) + "/" + saveDir + "/"
+        GenerateRichDataFromTheta(rs,el,saveName,thetaName,repeat,True)
+    print("CMAES:End of generation")
+
+def generateFromRegression(repeat, rs, saveDir):
+    thetaName = rs.path + rs.thetaFile
+    saveName = rs.path + saveDir + "/"
+    GenerateDataFromTheta(rs,0.05,saveName,thetaName,repeat,True)
+    print("Regression:End of generation")
+
+def generateRichDataFromRegression(repeat, rs,thetaFile, saveDir):
+    thetaName = rs.path + thetaFile
+    saveName = rs.path + saveDir + "/"        
+    GenerateRichDataFromTheta(rs,0.05,saveName,thetaName,repeat,True)
+    print("Regression:End of generation")
+
+def launchCMAESForSpecificTargetSize(target_size, rs, save):
+    '''
+    Run cmaes for a specific target size
+
+    Input:	
+            -target_size, size of the target, float
+            -setuFile, file of setup, string
+            -save: do we use a previous Best.theta file? True = Yes, False = use current controller, None = random controller
+    '''
+    print("Starting the CMAES Optimization for target " + str(target_size) + " !")
+    foldername = rs.OPTIpath + str(target_size) + "/"
+    if save:
+        thetaname = foldername + rs.thetaFile
+        copyRegressiontoCMAES(rs, rs.thetaFile, target_size)
+    else:
+        thetaname = foldername + "Best"
+
+    #Initializes all the class used to generate trajectory
+    exp = Experiments(rs, target_size, False, foldername, thetaname,rs.popsizeCmaes,rs.period)
+    theta = exp.tm.controller.getTheta()
+    thetaCMA = theta.flatten()
+
+    #run the optimization (cmaes)
+    cma.fmin(exp.runTrajectoriesCMAES, thetaCMA, rs.sigmaCmaes, options={'maxiter':rs.maxIterCmaes, 'popsize':rs.popsizeCmaes, 'CMA_diagonal':True, 'verb_log':50, 'verb_disp':1,'termination_callback':term()})
+    print("End of optimization for target " + str(target_size) + " !")
+    
+def launchCMAESForSpecificTargetSizeAndSpecificPoint(target_size, rs, save, point, noise=None):
+    '''
+    Run cmaes for a specific target size
+
+    Input:    -target_size, size of the target, float
+            -setuFile, file of setup, string
+            -save: do we use a previous Best.theta file? True = Yes, False = use current controller, None = random controller
+            noise: noise on muscle, if None, defalt noise from muscle setup, float
+    '''
+    pos=point[0]
+    x=point[1][0]
+    y=point[1][1]
+    print("Starting the CMAES Optimization for target " + str(target_size) + " for point "+ str(pos)+" !")
+    foldername = rs.OPTIpath + str(target_size)+"/"+str(pos)+"/"
+    
+
+    thetaname = foldername + "Best"
+    if save:
+        checkIfFolderExists(foldername)
+        copyfile(rs.OPTIpath + str(target_size)+"/" + "Best.theta",foldername + "Best.theta")
+    elif save==None:
+        thetaname=None
+
+
+
+
+    #Initializes all the class used to generate trajectory
+    exp = Experiments(rs, target_size, False, foldername, thetaname,rs.popsizeCmaes,rs.period)
+    if(noise!=None):
+        exp.setNoise(noise)
+    theta = exp.tm.controller.getTheta()
+    thetaCMA = theta.flatten()
+
+    #run the optimization (cmaes)
+    cma.fmin(partial(exp.runTrajectoriesCMAESOnePoint, x, y), thetaCMA, rs.sigmaCmaes, options={'maxiter':rs.maxIterCmaes, 'popsize':rs.popsizeCmaes, 'CMA_diagonal':True, 'verb_log':0, 'verb_disp':0,'termination_callback':term()})
+    print("End of optimization for target " + str(target_size) +  " for point "+ str(pos)+" !")
+    
+    
+def launchCMAESForAllTargetSizes(rs, save):
+    for el in rs.target_size:
+        launchCMAESForSpecificTargetSize(el, rs,save)
+
+def term():
+    return False
+
+def checkAllPoint(rs, sizeTarget):
+    posIni= np.loadtxt(pathDataFolder + rs.experimentFilePosIni)
+    print("check CMAES progression for target " + str(sizeTarget) + " : ")
+    print("Point    |    max    |    gap between max and min ")
+    maxR = np.zeros(posIni.shape[0])
+    gap= np.zeros(posIni.shape[0])
+    for i in range(posIni.shape[0]):
+        name = rs.OPTIpath + str(sizeTarget)+"/"+str(i)+ "/Cost/cmaesCost.log"
+        data = np.loadtxt(name)
+        maxR[i]=data[:,-1].max()
+        gap[i]=data[-1][-1]-data[-1][0]
+        print(str(i)+"        "+str(maxR[i])+"        "+str(gap[i]))
+
+    
+
+
+        
+        
+
+#--------------------------- multiprocessing -------------------------------------------------------
+
+def lauchCMAESForListOfPoints(target_size, rs, save, points):
+    p = ThreadPool(processes=len(points))
+    posIni = np.loadtxt(pathDataFolder + rs.experimentFilePosIni)
+    p.map(partial(launchCMAESForSpecificTargetSizeAndSpecificPointMulti, target_size, rs, save), [[i, posIni[i]] for i in points])
+    p.close()
+    p.join()
+    
+def launchCMAESForSpecificTargetSizeAndSpecificPointMulti(target_size, rs, save, point):
+    '''
+    Run cmaes for a specific target size
+
+    Input:    -target_size, size of the target, float
+            -setuFile, file of setup, string
+            -save: do we use a previous Best.theta file? True = Yes, False = use current controller, None = random controller
+    '''
+    pos=point[0]
+    x=point[1][0]
+    y=point[1][1]
+    print("Starting the CMAES Optimization for target " + str(target_size) + " for point "+ str(pos)+" !")
+    foldername = rs.OPTIpath + str(target_size)+"/"+str(pos)+"/"
+    
+
+    thetaname = foldername + "Best"
+    if save:
+        checkIfFolderExists(foldername)
+        copyfile(rs.OPTIpath + str(target_size)+"/" + "Best.theta",foldername + "Best.theta")
+    elif save==None:
+        thetaname=None
+
+
+
+
+    #Initializes all the class used to generate trajectory
+    exp = Experiments(rs, target_size, False, foldername, thetaname,rs.popsizeCmaes,rs.period)
+    theta = exp.tm.controller.getTheta()
+    thetaCMA = theta.flatten()
+
+    #run the optimization (cmaes)
+    cma.fmin(partial(exp.runTrajectoriesCMAESOnePointMulti, x, y), thetaCMA, rs.sigmaCmaes, options={'maxiter':rs.maxIterCmaes, 'popsize':rs.popsizeCmaes, 'CMA_diagonal':True, 'verb_log':0, 'verb_disp':0,'termination_callback':term()})
+    print("End of optimization for target " + str(target_size) +  " for point "+ str(pos)+" !")
+    
+def launchCMAESMissing(rs, save, gamma, point):
+    '''
+    Run cmaes for a specific target size
+
+    Input:    -target_size, size of the target, float
+            -setuFile, file of setup, string
+            -save: do we use a previous Best.theta file? True = Yes, False = use current controller, None = random controller
+    '''
+    rs.gamma = gamma/10.0
+    pos=point[0]
+    x=point[1][0]
+    y=point[1][1]
+    target_size = point[2]
+    print("Starting the CMAES Optimization for gamma = "+ str(gamma) + ",target " + str(target_size) + " for point "+ str(pos)+" !")
+    foldername = rs.OPTIpath + "gamma" + str(gamma) + "/"+str(target_size)+"/"+str(pos)+"/"
+
+    thetaname = foldername + "Best"
+    if save:
+        checkIfFolderExists(foldername)
+        copyfile(rs.OPTIpath + "gamma" + str(gamma) + "/" + str(target_size)+"/" + "Best.theta",foldername + "Best.theta")
+    elif save==None:
+        thetaname=None
+
+
+    #Initializes all the classes used to generate trajectory
+    exp = Experiments(rs, target_size, False, foldername, thetaname,rs.popsizeCmaes,rs.period)
+    theta = exp.tm.controller.getTheta()
+    thetaCMA = theta.flatten()
+
+    #run the optimization (cmaes)
+    cma.fmin(partial(exp.runTrajectoriesCMAESOnePointMulti, x, y), thetaCMA, rs.sigmaCmaes, options={'maxiter':rs.maxIterCmaes, 'popsize':rs.popsizeCmaes, 'CMA_diagonal':True, 'verb_log':0, 'verb_disp':0,'termination_callback':term()})
+    print("End of optimization for gamma = "+ str(gamma) + ",target " + str(target_size) + " for point "+ str(pos)+" !")
+
+
+def launchCMAESForAllPoint(rs, target_size, save, noise=None):
+    """
+        Launch in parallel (on differents processor) the cmaes optimization for each point
+        input:
+                    rs: setup file
+                    target_size: size of the target
+                    save: for save experience log
+                    noise: noise on muscle, if None, defalt noise from muscle setup
+    
+    """
+    p = ThreadPool(processes=15)
+    #run cmaes on each targets size on separate processor
+    posIni = np.loadtxt(pathDataFolder + rs.experimentFilePosIni)
+    p.map(partial(launchCMAESForSpecificTargetSizeAndSpecificPoint, target_size, rs, save, noise=noise), enumerate(posIni))
+    p.close()
+    p.join()
+    
+def launchCMAESForAllTargetSizesMulti(rs):
+    '''
+    Launch in parallel (on differents processor) the cmaes optimization for each target size
+    '''
+    #initializes setup variables
+    #initializes a pool of worker, ie multiprocessing
+    p = ThreadPool(processes=4)
+    #run cmaes on each targets size on separate processor
+    p.map(partial(launchCMAESForSpecificTargetSize, rs=rs, save=False), rs.target_size)
+    p.close()
+    p.join()
---
+++ Control/runCMAES.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Author: Thomas Beucher + Olivier Sigaud
+
+Module: runScript
+
+Description: script to run cmaes
+'''
+
+
+
+
+from Main.MainCMAES import lauchCMAESForListOfPoints, checkAllPoint, launchCMAESForSpecificTargetSizeAndSpecificPointMulti, generateFromCMAESonePoint, generateFromCMAESNController, launchCMAESForAllPoint, launchCMAESForAllTargetSizesMulti, generateFromCMAES, generateRichDataFromRegression, generateRichDataFromCMAES, launchCMAESForAllTargetSizes, launchCMAESForSpecificTargetSize
+
+from Plot.plotFunctions import plotCostColorMapFor12, plotCMAESOnePointProgress, plotEstimatorPoint, trajectoriesAnimation, plotCostColorMap, plotTimeColorMap, plotTimeDistanceTarget, plotFittsLaw, plotPerfSizeDist, plotVelocityProfile, plotXYPositions, plotXYEstimError, plotXYEstimErrorOfSpeed, plotArticularPositions, plotInitPos, plotMuscularActivations, plotScattergram, plotHitDispersion, plotExperimentSetup, plotCMAESProgress, plotTrajsInRepo, plotManipulability, plotManipulability2
+from GlobalVariables import pathDataFolder
+import numpy as np
+from Utils.Chrono import Chrono
+from Utils.ReadXmlFile import ReadXmlFile
+
+
+#----------------------------- main list of available actions ----------------------------------------------------------------------
+
+def printMainMenu():
+    print('    CMAES:')
+    print('        1 train CMAES for all targets')
+    print('        2 generate results from CMAES controllers')
+    print('        3 plot velocity profiles')
+    print('        4 plot XY and articular positions')
+    print('        5 plot muscular activations')
+    print('        6 plot cost Map')                  
+    print('        7 plot Time x Distance for Targets')                  
+    print('        8 plot Size x Dist')                  
+    print('        9 plot Fitts Law')                  
+    print('        10 plot Map Time x Trajectory')
+    print('        11 show trajectory animations (all)')                 
+    print('        12 plot Hit dispersion (CMAES or RBFN)')
+    print('        13 train CMAES for one target')
+    print('        14 plot CMAES cost progress')
+    print('        15 generate rich results from RBFN controller')
+    print('        16 generate rich results from CMAES controllers')
+    print('        17 plot successful trajectories in repository')
+    print('        18 plot XY estimation error')
+    print('        19 plot Estimation error as function of velocity')
+    print('        20 plot Experimental set-up')
+    print('        21 plot Directional Manipulability')
+    print('        22 plot Manipulability')
+    print('        23 plot Estimation')
+    print('        24 train CMAES one point')
+    print('        25 train CMAES one point for a list of points')
+    print('        26 plot CMAES One point cost progress')
+    print('        27 generate results from CMAES One point')
+    print('        28 plot XY  for one target')  
+    print('        29 generate results for one target')
+    print('        30 plot cost Map of muscles 1 and 2')
+    print('        31 check CMAES progression')
+    
+def runChoice():
+    checkL = True
+    fileName = raw_input('Name of file to load setup : ')
+    rs = ReadXmlFile(fileName)
+    while checkL:
+        printMainMenu()
+        choix = input('Enter the number corresponding to the script you want to run: ')
+        choix = int(choix)
+        if(chooseFunction(choix, rs)==0): break
+
+def chooseFunction(choix, rs):
+    if choix == 1:
+        rorc = input("enter 1 if from Regression, anything if from previous CMAES: ")
+        save = False
+        rorc = int(rorc)
+        if rorc == 1:
+            save = True
+        c = Chrono()
+        launchCMAESForAllTargetSizesMulti(rs)
+        c.stop()
+    elif choix == 2:
+        #TODO: Choose the kinematic model
+        nameTheta = raw_input('Name of the controller file: ')
+        name = raw_input('Folder where you want to save the results: ')
+        nbret = input("Number of repeat for each trajectory (int): ")
+        nbret = int(nbret)
+        generateFromCMAES(nbret, rs, nameTheta, name)
+    elif choix == 3:
+        nameF = raw_input('Folder where the results are saved: ')
+        plotVelocityProfile("OPTI",rs,nameF)
+    elif choix == 4:
+        nameF = raw_input('Folder where the results are saved: ')
+        rorc = input("enter 1 if XY or 2 if Joint results: ")
+        rorc = int(rorc)
+        if rorc == 1:
+            plotXYPositions("OPTI",rs, nameF,"All",False)
+        else:
+            plotArticularPositions("OPTI",rs, nameF)
+    elif choix == 5:
+        nameF = raw_input('Folder where the results are saved: ')
+        tSize = raw_input('Target Size: ')
+        plotMuscularActivations("OPTI",rs,nameF,tSize)
+    elif choix == 6:
+        nameF = raw_input('Folder where the results are saved: ')
+        #tSize = raw_input('Target Size: ')
+        #plotCostColorMap("OPTI",nameF,tSize)
+        plotCostColorMap("OPTI",rs, nameF)
+    elif choix == 7:
+        nameF = raw_input('Folder where the results are saved: ')
+        plotTimeDistanceTarget(nameF, rs)
+    elif choix == 8:
+        nameF = raw_input('Folder where the results are saved: ')
+        plotPerfSizeDist(nameF, rs)
+    elif choix == 9:
+        nameF = raw_input('Folder where the results are saved: ')
+        plotFittsLaw(nameF, rs)
+    elif choix == 10:
+        nameF = raw_input('Folder where the results are saved: ')
+        plotTimeColorMap("OPTI",rs, nameF)
+    elif choix == 11:
+        rorc = input("enter 0 if Brent, 1 if Regression or 2 if CMAES results: ")
+        rorc = int(rorc)
+        if rorc == 0:
+            trajectoriesAnimation("Brent", rs)
+        elif rorc == 1:
+            nameF = raw_input('Folder where the results are saved: ')
+            trajectoriesAnimation("RBFN",rs, nameF)
+        elif rorc == 2:
+            nameF = raw_input('Folder where the results are saved: ')
+            tSize = raw_input('Target Size: ')
+            trajectoriesAnimation("OPTI",rs, nameF, tSize)
+    elif choix == 12:
+        nameF = raw_input('Folder where the results are saved: ')
+        rorc = input("enter 1 if RBFN or 2 if CMAES results: ")
+        #plotHitDispersion(nameF,"0.05")
+        rorc = int(rorc)
+        if rorc == 1:
+            plotScattergram("RBFN",nameF, rs)
+        elif rorc == 2:
+            plotScattergram("OPTI",nameF, rs)
+    elif choix == 13:
+        rorc = input("enter 1 if from RBFN, anything if from previous CMAES: ")
+        save = False
+        rorc = int(rorc)
+        if rorc == 1:
+            save = True
+        tSize = raw_input('Target Size: ')
+        c = Chrono()
+        launchCMAESForSpecificTargetSize(float(tSize),rs,save)
+        c.stop()
+    elif choix == 14:
+        plotCMAESProgress(rs)
+    elif choix == 15:
+        name = raw_input('Name of the Regression controller file: ')
+        fname = raw_input('Folder where you want to save the results: ')
+        nbret = input("Number of repeat for each trajectory (int): ")
+        c = Chrono()
+        generateRichDataFromRegression(nbret,rs, name, fname)
+        c.stop()
+    elif choix == 16:
+        nameTheta = raw_input('Name of the controller file: ')
+        name = raw_input('Folder where you want to save the results: ')
+        nbret = input("Number of repeat for each trajectory (int): ")
+        nbret = int(nbret)
+        c = Chrono()
+        generateRichDataFromCMAES(nbret,rs, nameTheta, name)
+        c.stop()
+    elif choix == 17:
+        plotTrajsInRepo()
+    elif choix == 18:
+        nameF = raw_input('Folder where the results are saved: ')
+        plotXYEstimError("OPTI",rs,nameF,"All")
+    elif choix == 19:
+        nameF = raw_input('Folder where the results are saved: ')
+        plotXYEstimErrorOfSpeed("OPTI",rs,nameF,"All")
+    elif choix == 20:
+        plotExperimentSetup(rs)
+    elif choix == 21:
+        plotManipulability(rs)
+    elif choix == 22:
+        plotManipulability2(rs)
+    elif choix == 23:
+        plotEstimatorPoint(rs, 0.04, 12)
+    elif choix == 24:
+        rorc = input("enter 1 if General CMAES, 2 if from scratch, anything if from previous CMAES point: ")
+        save = False
+        rorc = int(rorc)
+        if rorc == 1:
+            save = True
+        elif rorc==2:
+            save=None
+        tSize = raw_input('Target Size: ')
+        c = Chrono()
+        launchCMAESForAllPoint(rs,float(tSize),save)
+        c.stop()
+    elif choix == 25:
+        #TODO: Test!
+        rorc = input("enter 1 if General CMAES, 2 if from scratch, anything if from previous CMAES point: ")
+        save = False
+        rorc = int(rorc)
+        if rorc == 1:
+            save = True
+        elif rorc==2:
+            save=None
+        tSize = raw_input('Target Size: ')
+        points=map(int, raw_input('Point(s): ').split())
+        print points
+        lauchCMAESForListOfPoints(float(tSize), rs, save, points)
+    elif choix == 26:
+        size=raw_input('Target Size: ')
+        while True:
+            print("    Enter the number of the point you want sea, q for quit")
+            point=raw_input('Point: ')
+            if(point=="q"): 
+                break
+            plotCMAESOnePointProgress(rs,size, point)
+    elif choix == 27:
+        #TODO: Choose the kinematic model
+        nameTheta = raw_input('Name of the controller file: ')
+        name = raw_input('Folder where you want to save the results: ')
+        nbret = input("Number of repeat for each trajectory (int): ")
+        nbret = int(nbret)
+        generateFromCMAESNController(nbret, rs, nameTheta, name)
+    elif choix == 28:
+        nameF = raw_input('Folder where the results are saved: ')
+        rorc = raw_input("Target Size: ")
+        plotXYPositions("OPTI",rs, nameF,rorc,False)
+    elif choix == 29:
+        nameTheta = raw_input('Name of the controller file: ')
+        name = raw_input('Folder where you want to save the results: ')
+        nbret = input("Number of repeat for each trajectory (int): ")
+        nbret = int(nbret)
+        size=raw_input('Target Size: ')
+        point=raw_input('Point: ')
+        generateFromCMAESonePoint(nbret, rs, nameTheta, name, float(size), point)
+    elif choix == 30:
+        nameF = raw_input('Folder where the results are saved: ')
+        #tSize = raw_input('Target Size: ')
+        #plotCostColorMap("OPTI",nameF,tSize)
+        plotCostColorMapFor12("OPTI",rs, nameF) 
+    elif choix == 31:
+        size=raw_input('Target Size: ')
+        checkAllPoint(rs, size)
+    else :
+        return 0
+    return 1
+
+
+runChoice()
