2c315cd393cb27010291c4657b717b0118107c53
Olivier Sigaud <olivier.sigaud@upmc.fr>
2016-08-22 10:37:44 +0200

    fixed script missing

--- Control/ScriptClusterMissing.sh
+++ Control/ScriptClusterMissing.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#PBS -N arm_model$TARGET
-#PBS -o arm_model$TARGET.out
-#PBS -b arm_model$TARGET.err
+#PBS -N missing$GAMMA
+#PBS -o missing$GAMMA.out
+#PBS -b missing$GAMMA.err
 #PBS -l walltime=48:00:00
 #PBS -l ncpus=15
 #PBS -V
---
+++ Control/testsubMissing.sh
@@ -0,0 +1,4 @@
+for gamma in 3 4 5 6 7 8 9
+do
+	python runMissing.py setupMissing.xml $gamma
+done
