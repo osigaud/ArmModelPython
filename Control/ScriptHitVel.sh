978b8fef332bf3202a9ca29781b57f6cdc7c4845
Olivier Sigaud <olivier.sigaud@upmc.fr>
2016-08-22 14:56:43 +0200

    adding hit velocity study

---
+++ Control/ScriptHitVel.sh
@@ -0,0 +1,4 @@
+for TARGET in 0.005 0.01 0.02 0.04
+do
+    python clusterOneTargetNController.py setupHitVelgamma6.xml $TARGET
+done
---
+++ Control/qsubHitVel.sh
@@ -0,0 +1,4 @@
+for target in 0.04 0.02 0.01 0.005
+do
+	qsub -v TARGET=$target ScriptClusterHitVel.sh
+done
