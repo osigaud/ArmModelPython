3e878bc318d0bdc51c4bd5b37bb316df7e1b5886
sigaud <sigaud@cluster.isir.upmc.fr>
2016-08-23 15:11:42 +0200

    improved qsub scripts from ludo

--- Control/ScriptClusterHitVel.sh
+++ Control/ScriptClusterHitVel.sh
@@ -3,7 +3,7 @@
 #PBS -o arm_model$TARGET.out
 #PBS -b arm_model$TARGET.err
 #PBS -l walltime=48:00:00
-#PBS -l ncpus=15
+#PBS -l nodes=1:ppn=16
 #PBS -V
 #PBS -d /home/sigaud/ArmModelPython/Control/
 python clusterOneTargetNController.py setupHitVelgamma6.xml $TARGET
--- Control/ScriptClusterMissing.sh
+++ Control/ScriptClusterMissing.sh
@@ -3,7 +3,7 @@
 #PBS -o missing$GAMMA.out
 #PBS -b missing$GAMMA.err
 #PBS -l walltime=48:00:00
-#PBS -l ncpus=15
+#PBS -l nodes=1:ppn=16
 #PBS -V
 #PBS -d /home/sigaud/ArmModelPython/Control/
 python runMissing.py setupMissing.xml $GAMMA
