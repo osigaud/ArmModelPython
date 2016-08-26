d87e94819291c6af4ea9050d270c85ad983cf775
Olivier Sigaud <olivier.sigaud@upmc.fr>
2016-08-26 14:23:50 +0200

    added missing xml files: bug fixed

--- Control/Utils/ReadXmlFile.py
+++ Control/Utils/ReadXmlFile.py
@@ -91,7 +91,7 @@ class ReadXmlFile(object):
     def CMAESParse(self, cmaesElement):
         self.maxIterCmaes           =int  (cmaesElement[0].text)
         self.numberOfRepeatEachTraj =int  (cmaesElement[1].text)
-        self.OPTIpath=path.abspath(path.expanduser(cmaesElement[2].text))+"/"
+        self.OPTIpath=path.abspath(path.expanduser(cmaesElement[2].text))
         self.sigmaCmaes             =float(cmaesElement[3].text)
         self.popsizeCmaes           =int  (cmaesElement[4].text)
         
